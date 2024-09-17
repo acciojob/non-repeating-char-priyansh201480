70590ea0c42ca60bb1793a3aa735947c18e778f9
Priyansh Agarwal <140069604+priyansh201480@users.noreply.github.com>
2024-09-17 23:23:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+ const charfreqcount={};
+
+	for(let char of str){
+		
+		if(charfreqcount[char]){
+			charfreqcount[char]++
+		}
+		else{
+			charfreqcount[char]=1
+		}
+		
+	}
+	for(let char of str){
+		if(charfreqcount[char] === 1){
+			return char;
+		}
+
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
